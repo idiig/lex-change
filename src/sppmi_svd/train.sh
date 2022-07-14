bccd1a2f8548b77500e27ffa820ec8583da626b5
idiig <avionplat@hotmail.com>
2022-07-15 00:36:53 +0900

    changed path

--- src/sppmi_svd/train.sh
+++ src/sppmi_svd/train.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-python counter_from_doc.py -f ../../data/before.txt -c ../../data/before.pkl
-python counter_from_doc.py -f ../../data/after.txt -c ../../data/after.pkl
-python id2word_from_counter.py --count_dic after.pkl before.pkl --threshold 0
-python main.py --file_path after.txt \
-    --name after \
+python counter_from_doc.py -f ../../cache/before.txt -c before.pkl
+python counter_from_doc.py -f ../../cache/after.txt -c after.pkl
+
+python id2word_from_counter.py --count_dic after.pkl before.pkl \
+       --threshold 0
+
+python main.py --file_path ../../cache/before.txt \
+    --name before \
     --pickle_id2word dic_id2word.pkl \
     --threshold 0 --has_cds --window_size 2 --shift 1 
+python main.py --file_path ../../cache/after.txt \
+    --name after \
+    --pickle_id2word dic_id2word.pkl \
+    --threshold 0 --has_cds --window_size 2 --shift 1
+
