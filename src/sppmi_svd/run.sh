18614c1bc684a5ba0e31dcafc6d2c8f82b4b0fde
idiig <avionplat@hotmail.com>
2022-07-14 22:37:38 +0900

    added workflow

--- src/sppmi_svd/run.sh
+++ src/sppmi_svd/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
-python main.py --file_path //sub_after_3.txt \
-    --name sub_after_3 \
+python counter_from_doc.py -f ../../data/before.txt -c ../../data/before.pkl
+python counter_from_doc.py -f ../../data/after.txt -c ../../data/after.pkl
+python id2word_from_counter.py --count_dic after.pkl before.pkl --threshold 0
+python main.py --file_path after.txt \
+    --name after \
     --pickle_id2word dic_id2word.pkl \
     --threshold 0 --has_cds --window_size 2 --shift 1 
-
-
